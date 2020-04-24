829856a32eecb3a9d8e1750c443bf875dfcbfdd2
DuffManLight <androidbuilder@gmail.com>
2020-04-24 13:36:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+
+using Newtonsoft.Json;
+
+namespace db_test_console_app
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables("TEST_")
+                .AddCommandLine(args)
+                .Build();
+
+            string temp = ConfigurationManager.AppSettings["MyFirstKey"];
+            var temp1 = configuration.GetSection("author");
+            var temp2 = configuration.GetSection("author:name");
+            var temp3 = configuration.GetSection("name");
+            var temp4 = configuration.GetSection("author").GetChildren();
+            var temp5 = configuration.GetSection("author:name").GetChildren();
+            var temp6 = configuration.GetSection("input");
+            var temp7 = configuration.GetSection("output");
+            var temp8 = configuration.Get<AppConfig>();
+            var temp9 = configuration.GetSection("author").Get<Author>();
+        }
+
+        [JsonObject("author")]
+        public class Author
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            // public string LastName { get; set; }
+        }
+
+        public class AppConfig
+        {
+            public ConnectionStringsConfig ConnectionStrings { get; set; }
+            public ApiSettingsConfig ApiSettings { get; set; }
+
+            public class ConnectionStringsConfig
+            {
+                public string MyDb { get; set; }
+            }
+
+            public class ApiSettingsConfig
+            {
+                public string Url { get; set; }
+                public string ApiKey { get; set; }
+                public bool UseCache { get; set; }
+            }
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,16 @@
+﻿ConfigurationManager
+https://benfoster.io/blog/net-core-configuration-legacy-projects
+https://espressocoder.com/2018/12/03/build-a-console-app-in-net-core-like-a-pro/
+
+JSON
+https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-how-to
+https://codeburst.io/working-with-json-in-net-core-3-2fd1236126c1
+
+SQL Client
+https://devblogs.microsoft.com/dotnet/introducing-the-new-microsoftdatasqlclient/
+
+.NET Core dependency injection:
+https://dzone.com/articles/dependency-injection-in-net-core-console-applicati
+https://martinfowler.com/articles/injection.html
+https://autofac.readthedocs.io/en/latest/getting-started/index.html
+https://andrewlock.net/using-dependency-injection-in-a-net-core-console-application/
